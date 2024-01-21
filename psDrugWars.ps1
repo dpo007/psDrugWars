1ba80e94646a5a6b0a44fa6a0a192df14adc8710
techie007 <11600782+dpo007@users.noreply.github.com>
2024-01-21 18:31:52 -0500

    Added new title screen 'graphic'

--- psDrugWars.ps1
+++ psDrugWars.ps1
@@ -3394,6 +3394,32 @@ function ShowTitleScreen {
             '....   iQBBBQ. .PBQBBBQ:   ...  :bQBQBBBBBBBv    KBBBi ....  .7ZBBQBBBBBL    .',
             '.....            .r7r.    .....               .       .....         .       ..',
             '........     . .       ...........           ....    ......... .         .....'
+        ),
+        @(
+            '                       .',
+            '     .__               M     .  .',
+            '     |  \._.. . _     dM     |  | _.._. __',
+            '     |__/[  (_|(_]    MMr    |/\|(_][  _)',
+            '               ._|   4MMML                  .',
+            '                     MMMMM.                xf',
+            '     .              "MMMMM               .MM-',
+            '      Mh..          +MMMMMM            .MMMM',
+            '      .MMM.         .MMMMML.          MMMMMh',
+            '       )MMMh.        MMMMMM         MMMMMMM',
+            '        3MMMMx.     ''MMMMMMf      xnMMMMMM"',
+            '        ''*MMMMM      MMMMMM.     nMMMMMMP"',
+            '          *MMMMMx    "MMMMM\    .MMMMMMM=',
+            '           *MMMMMh   "MMMMM"   JMMMMMMP',
+            '             MMMMMM   3MMMM.  dMMMMMM            .',
+            '              MMMMMM  "MMMM  .MMMMM(        .nnMP"',
+            '  =..          *MMMMx  MMM"  dMMMM"    .nnMMMMM*',
+            '    "MMn...     ''MMMMr ''MM   MMM"   .nMMMMMMM*"',
+            '     "4MMMMnn..   *MMM  MM  MMP"  .dMMMMMMM""',
+            '       ^MMMMMMMMx.  *ML "M .M*  .MMMMMM**"',
+            '          *PMMMMMMhn. *x > M  .MMMM**""',
+            '             ""**MMMMhx/.h/ .=*"',
+            '                      .3P"%....',
+            '                    nP"     "*MMnx'
         )
     )
     
